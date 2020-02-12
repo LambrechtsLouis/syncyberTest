fd16a9f629386a4be82afa28efdfbf7cec6a78e7
LambrechtsLouis <60960809+LambrechtsLouis@users.noreply.github.com>
2020-02-12 12:51:03 +0100

    Update opstart.sh

--- scripts/opstart.sh
+++ scripts/opstart.sh
@@ -30,10 +30,15 @@ FLUSH PRIVILEGES;
 CREATE DATABASE syncyber;
 CREATE USER 'administrator'@'localhost' IDENTIFIED BY 'R1234-56'; 
 GRANT ALL PRIVILEGES ON *.* TO 'administrator'@'localhost' WITH GRANT OPTION;
+
 CREATE USER 'applicatie'@'localhost' IDENTIFIED BY 'applicatie'; 
 GRANT ALL PRIVILEGES ON *.* TO 'applicatie'@'localhost' WITH GRANT OPTION;
-CREATE USER 'microcontroller'@'localhost' IDENTIFIED BY 'microcontroller'; 
+
+CREATE USER 'microcontroller'@'localhost' IDENTIFIED BY 'microcontroller';
 GRANT ALL PRIVILEGES ON *.* TO 'microcontroller'@'localhost' WITH GRANT OPTION;
+
+CREATE USER 'gregory'@'%' IDENTIFIED BY 'badmuts'; 
+GRANT ALL PRIVILEGES ON *.* TO 'gregory'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
 MYSQL_SCRIPT
 
@@ -42,6 +47,7 @@ sudo apt-get install php libapache2-mod-php -y
 #Give rights to apache www folder 
 sudo mkdir /var/www/ 
 sudo chmod -R 777 /var/www/
+sudo chmod -R 777 /var/www/html
 
 #installatie van PHPMyAdmin
 sudo apt install phpmyadmin php-mbstring php-gettext -y
@@ -54,30 +60,13 @@ sudo systemctl restart apache2
 sudo a2enmod rewrite -y
 sudo systemctl restart apache2
 
-#installeer python
-sudo apt-get install python3 -y
-
-#installeer pip
-sudo apt-get install python3-pip -y
-
-#installeer flask
-pip3 install Flask
-
-#installeer DASH
-pip3 install dash
-
-#install mysql connector
-pip3 install mysql-connector
-
 #mappen aanmaken
-sudo mkdir /home/administrator/dashboard
-sudo chmod -R 777 /home/administrator/dashboard
 sudo mkdir /home/administrator/backup
 sudo chmod -R 777 /home/administrator/backup
 
 #create mount point for usb
-sudo mkdir /media/usb
-sudo chown -R administrator:administrator /media/usb
+sudo mkdir /media/backup
+sudo chown -R administrator:administrator /media/backup
 
 
 #add a virtual host to apache2
@@ -95,6 +84,8 @@ sudo service apache2 restart
 sudo aptitude install automysqlbackup autopostgresqlbackup -y
 mkdir /opt/automysqlbackup
 cd /opt/automysqlbackup/
+mkdir /var/backup/
+chmod 777 /var/backup
 wget http://ufpr.dl.sourceforge.net/project/automysqlbackup/AutoMySQLBackup/AutoMySQLBackup%20VER%203.0/automysqlbackup-v3.0_rc6.tar.gz
 tar zxf automysqlbackup-v3.0_rc6.tar.gz
 sudo ./install.sh
@@ -102,7 +93,43 @@ cd /etc/automysqlbackup/
 #nano myserver.conf aanpassen van de instellingen aan de database
 
 #schrijven en aanmaken van backupscript
-sudo bash -c 'echo -e "#!/bin/bash\nbackup_files=\"/home /var /etc \"\ndest=\"/media/usb/backup \"\nday=\$(date+%A)\nhostname=\$(hostname -s)\narchive_file=\"\$hostname-\$day.tgz\"\necho \"Backing up \$backup_files to \$dest/\$archive_file \"\ntar czf \$dest/\$archive_file \$backup_files \n echo \"backup finished\"\nls -lh \$dest"' > /home/administrator/backup/backup.sh
+sudo bash -c 'echo -e "#!/bin/bash\nbackup_files=\"/home /var /etc \"\ndest=\"/media/backup\"\nday=\$(date+%A)\nhostname=\$(hostname -s)\narchive_file=\"\$hostname-\$day.tgz\"\necho \"Backing up \$backup_files to \$dest/\$archive_file \"\ntar czf \$dest/\$archive_file \$backup_files \n echo \"backup finished\"\nls -lh \$dest"' > /home/administrator/backup/backup.sh
 
 alias backup='sudo /home/administrator/backup/backup.sh'
 sudo chmod u+x /home/administrator/backup/backup.sh
+
+#crontabfile instellen
+sudo bash -c 'echo -e "39 10	* * *	root	/usr/local/bin/automysqlbackup /etc/automysqlbackup/myserver.conf"' > /etc/crontab
+sudo bash -c 'echo -e "22 11    * * *   root    /home/administrator/backup/backup.sh"' > /etc/crontab
+
+#crontab -e instellen
+crontab -l | { cat; echo "47 10 * * * /usr/local/bin/automysqlbackup /etc/automysqlbackup/myserver.conf"; } | crontab -
+crontab -l | { cat; echo "22 11 * * * /home/administrator/backup/backup.sh"; } | crontab -
+
+#ports apache instellen
+sudo bash -c 'echo -e "Listen 8050"' > /etc/apache2/ports.conf
+
+#instellen van firewall rules
+sudo apt-get install ufw
+
+sudo ufw disable
+
+sudo ufw default deny incoming
+sudo ufw default deny outgoing
+
+sudo ufw allow 22/tcp
+sudo ufw allow 22
+sudo ufw allow 80
+sudo ufw allow 443
+sudo ufw allow 8050
+sudo ufw allow 5000:5001/tcp
+sudo ufw allow 5000:5001/udp
+sudo ufw allow 3306/udp
+sudo ufw allow 3306/tcp
+
+sudo ufw allow out 8050
+sudo ufw allow out 80
+sudo ufw allow out 443
+sudo ufw allow out 3306
+
+sudo ufw enable
